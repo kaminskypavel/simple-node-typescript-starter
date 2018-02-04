b6f37246af19df43597324fdf21173aa533a4d66
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2018-02-05 01:04:43 +0200

    feat: add isDevelopment method

--- src/commons/index.ts
+++ src/commons/index.ts
@@ -4,14 +4,22 @@
 import chalk, {Chalk} from "chalk";
 import * as moment from "moment";
 
+process.env = process.env || 'development';
+
+export const isDevelopment = (): boolean => {
+	return process.env.NODE_ENV === 'development';
+};
+
 export const log = (msg: any, color?: Chalk) =>
-    console.log(`[${chalk.yellow.bold(moment().format('DD-MM-YY h:mm:ss'))}]`,
-        (color || chalk.blue.bold)(JSON.stringify(msg)));
+	console.log(`[${chalk.yellow.bold(moment().format('DD-MM-YY h:mm:ss'))}]`,
+		(color || chalk.blue.bold)(JSON.stringify(msg)));
 
 export const debug = (msg: any, color?: Chalk) => {
-        console.log(`[${chalk.yellow.bold(moment().format('DD-MM-YY h:mm:ss'))}]`, (color || chalk.greenBright.bold)(JSON.stringify(msg)));
+	if (isDevelopment()) {
+		console.log(`[${chalk.yellow.bold(moment().format('DD-MM-YY h:mm:ss'))}]`, (color || chalk.greenBright.bold)(JSON.stringify(msg)));
+	}
 };
 
 export const error = (msg: any, err: Error = new Error(), color?: Chalk) =>
-    console.log(`[${chalk.red(moment().format('DD-MM-YY h:mm:ss'))}]`,
-        (color || chalk.red)(msg), (color || chalk.red)(err.toString()));
+	console.log(`[${chalk.red(moment().format('DD-MM-YY h:mm:ss'))}]`,
+		(color || chalk.red)(msg), (color || chalk.red)(err.toString()));
