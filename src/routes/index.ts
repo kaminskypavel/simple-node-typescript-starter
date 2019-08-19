826af111155afa62914e0af48a24beeca28993b0
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-08-19 11:09:25 +0300

    feat: integrate global rateLimiter for all the requests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,14 +4,16 @@ import RateLimit from 'express-rate-limit';
 
 const limiter = new RateLimit({
 	windowMs: 15 * 60 * 1000, // 15 min timeframe
-	max: 100, // max-requests
+	max: 2000, // max-requests
 	delayMs: 0
 });
 
 export default (app: Express) => {
+	app.use(limiter);
+
 	app.use('/demo', demoRoute);
 
-	app.get('/', (req: Request, res: Response) => {
+	app.get('/', (req, res: Response) => {
 		res.jsend.success('ok');
 	});
 };
