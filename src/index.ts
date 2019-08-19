e060ab363355979e9b1586391fe2b6906f0245b3
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-08-19 11:17:54 +0300

    feat: add 'express-status-monitor' and integrate in _monitor route

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import chalk from 'chalk';
 import jsend from 'jsend';
 import routes from './routes';
+const statusMonitor = require('express-status-monitor')();
 
 dotenv.config();
 const app = express();
@@ -14,6 +15,7 @@ app.use(helmet({}));
 app.use(cors());
 app.use(cookieParser());
 app.use(jsend.middleware);
+app.use(statusMonitor);
 
 routes(app);
 
@@ -21,5 +23,5 @@ const port = process.env.PORT;
 app.listen(port, () => {
 	console.log(`simple-node-typescript-starter server is running`);
 	console.log(`navigate to http://localhost:${port}`);
-	console.log(chalk.yellow(`Please remember to rewise security`));
+	console.log(chalk.yellow(`Please remember to rewise security and disable /_monitor route`));
 });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import demoRoute from './demoRoute';
 import {Express, Request, Response} from 'express';
 import RateLimit from 'express-rate-limit';
+const statusMonitor = require('express-status-monitor')();
 
 const limiter = new RateLimit({
 	windowMs: 15 * 60 * 1000, // 15 min timeframe
@@ -12,6 +13,8 @@ export default (app: Express) => {
 	app.use(limiter);
 
 	app.use('/demo', demoRoute);
+	console.log(statusMonitor.pageRoute);
+	app.get('/_monitor', statusMonitor.pageRoute);
 
 	app.get('/', (req, res: Response) => {
 		res.jsend.success('ok');
