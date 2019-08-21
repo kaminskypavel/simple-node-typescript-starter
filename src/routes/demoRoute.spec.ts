0bf8b4e10eca38c0e6616d9eb666ff4284b969a8
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-08-21 12:30:18 +0300

    test: add tests for /qs path

--- src/routes/demoRoute.spec.ts
+++ src/routes/demoRoute.spec.ts
@@ -11,3 +11,25 @@ describe('GET /demo', function() {
 		});
 	});
 });
+
+describe('GET /demo/qs', function() {
+	it('responds with a correct message', async () => {
+		const response = await request(app).get('/demo/qs?name=Panda');
+		expect(response.status).toBe(200);
+		expect(JSON.parse(response.text)).toEqual({
+			status: 'success',
+			data: 'Hello Panda'
+		});
+	});
+
+	it('responds an error message if name query string is missing', async () => {
+		const response = await request(app).get('/demo/qs');
+		expect(response.status).toBe(200);
+		expect(JSON.parse(response.text)).toEqual(
+			expect.objectContaining({
+				status: 'error',
+				message: 'validation error'
+			})
+		);
+	});
+});
