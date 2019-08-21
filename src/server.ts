07ed4b05945913697033d78b35de3a8045d3a267
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2019-08-21 11:58:15 +0300

    fix: fix url printed once server is up

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const port = process.env.PORT;
 app.listen(port, () => {
 	logger.info(`simple-node-typescript-starter server is running`);
-	logger.info(`navigate to http://localhost:${port}`);
+	logger.info(`navigate to http://localhost:${port}/demo`);
 	logger.warn(
 		chalk.bgYellow(
 			`Please remember to disable monitoring and swagger in the .env file`
